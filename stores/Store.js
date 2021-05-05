c0fb1841aeefc95b119da6da8c5c25908b67215e
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-05 20:15:19 +0000

    Automatically backed up by Learn

--- stores/Store.js
+++ stores/Store.js
@@ -1,7 +1,4 @@
 class Store {
-  // Your implementation here.
-  // Hint: Our <App /> component won't directly use this store, but instead the
-  // CounterStore is going to inherit from it.
   constructor(initialState){
     this.state = initialState;
     this.listeners = [];
